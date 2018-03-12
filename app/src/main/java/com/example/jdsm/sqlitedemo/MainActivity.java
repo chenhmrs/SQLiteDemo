ba7fa31ed2216d074c915eb6a6a903d5428f0d02
chenhmrs <1825609506@qq.com>
2018-03-12 22:50:51 +0800

    home2

--- app/src/main/java/com/example/jdsm/sqlitedemo/MainActivity.java
+++ app/src/main/java/com/example/jdsm/sqlitedemo/MainActivity.java
@@ -1,15 +1,59 @@
 package com.example.jdsm.sqlitedemo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
     public MyDatabaseHelper dbHelper;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        dbHelper=new MyDatabaseHelper(this,"BookStore",null,1);
-        dbHelper.getWritableDatabase();
+        ButterKnife.bind(this);
+        dbHelper = new MyDatabaseHelper(this, "BookStore.db", null, 2);
+
+    }
+
+    @OnClick({R.id.button_create, R.id.button_insert, R.id.button_update, R.id.button_delete, R.id.button_query})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.button_create:
+                dbHelper.getWritableDatabase();
+                break;
+            case R.id.button_insert:
+                SQLiteDatabase db=dbHelper.getWritableDatabase();
+                ContentValues values=new ContentValues();
+                values.put("name","Book1");
+                values.put("author","A");
+                values.put("pages",454);
+                values.put("price",16.69);
+                db.insert("Book",null,values);
+                values.clear();
+                values.put("name","Book2");
+                values.put("author","B");
+                values.put("pages",124);
+                values.put("price",12.25);
+                db.insert("Book",null,values);
+                break;
+            case R.id.button_update:
+                SQLiteDatabase db1=dbHelper.getWritableDatabase();
+                ContentValues values1=new ContentValues();
+                values1.put("price",10.99);
+                db1.update("Book",values1,"name= ?",new String[]{"Book2"});
+                break;
+            case R.id.button_delete:
+                SQLiteDatabase db2=dbHelper.getWritableDatabase();
+                db2.delete("Book","pages> ?",new String[]{"400"});
+                break;
+            case R.id.button_query:
+                break;
+        }
     }
 }
--- app/src/main/java/com/example/jdsm/sqlitedemo/MyDatabaseHelper.java
+++ app/src/main/java/com/example/jdsm/sqlitedemo/MyDatabaseHelper.java
@@ -12,10 +12,14 @@ public class MyDatabaseHelper extends SQLiteOpenHelper{
      private Context mContext;
      public static final String CREATE_BOOK="create table Book ( "
              + "id integer primary key autoincrement, "
-             + "auto text, "
-             + "price real"
-             + "pages integer"
+             + "author text, "
+             + "price real,"
+             + "pages integer,"
              + "name text )";
+     public static final String CREATE_CATEGORY="create table Category("
+             +"id integer primary key autoincrement,"
+             +"category_name text,"
+             +"category_code integer)";
     /**
      *
      * @param context 估计最好用application的
@@ -30,11 +34,16 @@ public class MyDatabaseHelper extends SQLiteOpenHelper{
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-
+        //在数据库中添加表，不写就只有数据库无表
+        db.execSQL(CREATE_BOOK);
+        db.execSQL(CREATE_CATEGORY);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
+        //onCreate只会执行一次，除非在onUpgrade里再次调用，所以删除以前的表
+        db.execSQL("drop table if exists Book");
+        db.execSQL("drop table if exists Category");
+        onCreate(db);
     }
 }
